05f190060aa7cd4ff836c718135189eec1202eda
Martin Passing <github@passing.name>
2022-04-25 20:03:04 +0200

    apply black code style

--- dvb-vlc.py
+++ dvb-vlc.py
@@ -8,16 +8,16 @@ import xml.etree.ElementTree as ET
 categories = {
     "TV HD": {
         "m3u_path": "dvb/m3u/tvhd.m3u",
-        "logo_url": "http://tv.avm.de/tvapp/logos/hd"
+        "logo_url": "http://tv.avm.de/tvapp/logos/hd",
     },
     "TV SD": {
         "m3u_path": "dvb/m3u/tvsd.m3u",
-        "logo_url": "http://tv.avm.de/tvapp/logos"
+        "logo_url": "http://tv.avm.de/tvapp/logos",
     },
     "Radio": {
         "m3u_path": "dvb/m3u/radio.m3u",
-        "logo_url": "http://tv.avm.de/tvapp/logos/radio"
-    } 
+        "logo_url": "http://tv.avm.de/tvapp/logos/radio",
+    },
 }
 
 logo_replace = [
@@ -26,14 +26,26 @@ logo_replace = [
     ("ü", "ue"),
     ("ß", "ss"),
     ("[ /]+", "_"),
-    ("[.,-]", "")
+    ("[.,-]", ""),
 ]
 
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, default="dvb", metavar='HOSTNAME', help='hostname of FRITZ!WLAN Repeater DVB-C')
-    parser.add_argument('--output', type=str, default="dvb.xspf", metavar='FILENAME', help='output filename')
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="dvb",
+        metavar="HOSTNAME",
+        help="hostname of FRITZ!WLAN Repeater DVB-C",
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        default="dvb.xspf",
+        metavar="FILENAME",
+        help="output filename",
+    )
     return parser.parse_args()
 
 
@@ -46,30 +58,27 @@ def get_m3u_channels(m3u_url):
     # split m3u playlist
     lines = result.text.split("\n")
 
-    for l in range(1, len(lines)-1, 3):
+    for l in range(1, len(lines) - 1, 3):
         # title from 1st line
-        title = re.sub('^#EXTINF:0,', '', lines[l]).strip()
+        title = re.sub("^#EXTINF:0,", "", lines[l]).strip()
         # option from 2nd line
-        option = re.sub('^#EXTVLCOPT:', '', lines[l+1])
+        option = re.sub("^#EXTVLCOPT:", "", lines[l + 1])
         # url from 3rd line
-        url = lines[l+2]
-                
-        channels[title] = {
-            "option": option,
-            "url": url
-        }
+        url = lines[l + 2]
+
+        channels[title] = {"option": option, "url": url}
 
     return channels
 
 
 def get_channel_image(title, logo_url):
-    image=title.lower()
+    image = title.lower()
 
     # replace patterns in image name
     for pattern, repl in logo_replace:
         image = re.sub(pattern, repl, image)
 
-    logo_url='{}/{}.png'.format(logo_url, image)
+    logo_url = "{}/{}.png".format(logo_url, image)
 
     return logo_url
 
@@ -82,17 +91,19 @@ def add_playlist_logos(channels, url):
 def export_xspf_playlist(channel_lists, filename):
     # create playlist root element
     playlist = ET.Element("playlist")
-    playlist.set('xmlns', 'http://xspf.org/ns/0/')
-    playlist.set('xmlns:vlc', 'http://www.videolan.org/vlc/playlist/ns/0/')
-    playlist.set('version', '1')
+    playlist.set("xmlns", "http://xspf.org/ns/0/")
+    playlist.set("xmlns:vlc", "http://www.videolan.org/vlc/playlist/ns/0/")
+    playlist.set("version", "1")
 
     # create playlist title, tracklist and playlist tree
     ET.SubElement(playlist, "title").text = "DVB"
     tracklist = ET.SubElement(playlist, "trackList")
-    playlist_tree = ET.SubElement(playlist, "extension", application="http://www.videolan.org/vlc/playlist/0")
+    playlist_tree = ET.SubElement(
+        playlist, "extension", application="http://www.videolan.org/vlc/playlist/0"
+    )
 
     # use unique track ID so the playlist can reference the elements in the tracklist
-    track_id=0
+    track_id = 0
 
     for category, channels in channel_lists.items():
         # create node in playlist tree
@@ -106,18 +117,20 @@ def export_xspf_playlist(channel_lists, filename):
             ET.SubElement(track, "image").text = channel["image"]
 
             # add track extension
-            track_ext = ET.SubElement(track, "extension", application="http://www.videolan.org/vlc/playlist/0")
-            ET.SubElement(track_ext, "vlc:id").text=str(track_id)
-            ET.SubElement(track_ext, "vlc:option").text=channel["option"]
+            track_ext = ET.SubElement(
+                track, "extension", application="http://www.videolan.org/vlc/playlist/0"
+            )
+            ET.SubElement(track_ext, "vlc:id").text = str(track_id)
+            ET.SubElement(track_ext, "vlc:option").text = channel["option"]
 
             # add track to playlist tree
             ET.SubElement(playlist_node, "vlc:item", tid=str(track_id))
 
-            track_id+=1
+            track_id += 1
 
     # write xspf playlist to file
     tree = ET.ElementTree(playlist)
-    tree.write(filename, encoding='UTF-8', xml_declaration=True)
+    tree.write(filename, encoding="UTF-8", xml_declaration=True)
 
 
 def main():
@@ -127,7 +140,9 @@ def main():
     # download and convert m3u playlists
     channel_lists = {}
     for category, options in categories.items():
-        channels = get_m3u_channels('http://{}/{}'.format(args.host, options["m3u_path"]))
+        channels = get_m3u_channels(
+            "http://{}/{}".format(args.host, options["m3u_path"])
+        )
         add_playlist_logos(channels, options["logo_url"])
         channel_lists[category] = channels
 
@@ -135,5 +150,5 @@ def main():
     export_xspf_playlist(channel_lists, args.output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
